72788e8082f7350b79ed60714db333239526f393
Dmitri G. Brengauz <dmitri@momus.net>
2015-08-23 13:22:28 -0600

    Everything seems to work, but shows that I've been cheating: I didn't write the tests first ;-)

--- test_cachematrix.R
+++ test_cachematrix.R
@@ -1,7 +1,11 @@
-library(prog031) #Change this if you're using submodule
+library(rprog031) #Change this if you're using submodule
+#REM this file exists as a hard link between submodule and package test directory
+                                        # rprog031/tests/testthat/test_prog2.R
+                                        # rprog031/ProgrammingAssignment2/test_cachematrix.R
 context("caching an inverse of a matrix")
 
-test_that("the makeCacheMatrix function takes one argument", {
-    expect_equal(makeCacheMatrix(), NULL)
+test_that("makeCacheMatrix with no arguments returns a list", {
+    expect_equal(class(makeCacheMatrix()), "list")
+    
 
 })
